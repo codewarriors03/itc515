9fd5e0da5a4c2c88f1edd12fc27364b2cea19e2e
codewarriors03 <52955769+codewarriors03@users.noreply.github.com>
2019-08-16 14:30:36 +1000

    Enforced style guidelines code 'Method Name'
    Updates file and followed the Code Style Guidelines 'method name should be meaningful' enforced.

--- ITC205_201960_Asg2_Baseline_Code/FixBookControl.java
+++ ITC205_201960_Asg2_Baseline_Code/FixBookControl.java
@@ -9,59 +9,59 @@ public class FixBookControl {
 
 
 	public FixBookControl() {
-		this.library = library.INSTANCE(); //variable 'LIB' to 'library'
+		this.library = library.getInstance(); //variable 'LIB' to 'library',  method 'INSTANCE()' to 'getInstance()'
 		state = CONTROL_STATE.INITIALISED; //variable 'StAtE' to 'state'
 	}
 	
 	
-	public void Set_Ui(FixBookUI fixBookUi) {	//variable 'UI' to 'fixBookUi'
+	public void setFixBookUI(FixBookUI fixBookUi) {	//variable 'UI' to 'fixBookUi',  method 'Set_Ui()' to 'setFixBookUI()'
 		if (!state.equals(CONTROL_STATE.INITIALISED)) { //variable 'StAtE' to 'state'
 			throw new RuntimeException("FixBookControl: cannot call setUI except in INITIALISED state");
 		}	
 		this.fixBookUi = fixBookUi;	//variable 'UI' to 'fixBookUi'
-		fixBookUi.Set_State(FixBookUI.UI_STATE.READY);
+		fixBookUi.setState(FixBookUI.UI_STATE.READY); //method 'Set_State()' to 'setState()'
 		state = CONTROL_STATE.READY; //variable 'StAtE' to 'state'		 
 	}
 
 
-	public void Book_scanned(int bookId) {
+	public void bookScanned(int bookId) { //method 'Book_scanned()' to 'bookScanned()'
 		if (!state.equals(CONTROL_STATE.READY)) { //variable 'StAtE' to 'state'
 			throw new RuntimeException("FixBookControl: cannot call bookScanned except in READY state");
 		}	
-		curBook = library.Book(bookId); //variable 'Cur_Book' to 'curBook', variable 'LIB' to 'library'
+		curBook = library.getBook(bookId); //variable 'Cur_Book' to 'curBook', variable 'LIB' to 'library', method 'Book()' to 'getBook()'
 		
 		if (curBook == null) { //variable 'Cur_Book' to 'curBook'
 			fixBookUi.display("Invalid bookId"); //variable 'UI' to 'fixBookUi'
 			return;
 		}
-		if (!curBook.IS_Damaged()) { //variable 'Cur_Book' to 'curBook'
+		if (!curBook.isDamaged()) { //variable 'Cur_Book' to 'curBook', method 'IS_Damaged()' to 'isDamaged()'
 			fixBookUi.display("Book has not been damaged"); //variable 'UI' to 'fixBookUi'
 			return;
 		}
 		fixBookUi.display(curBook.toString()); //variable 'UI' to 'fixBookUi', variable 'Cur_Book' to 'curBook'
-		fixBookUi.Set_State(FixBookUI.UI_STATE.FIXING); //variable 'UI' to 'fixBookUi'
+		fixBookUi.setState(FixBookUI.UI_STATE.FIXING); //variable 'UI' to 'fixBookUi', method 'Set_State()' to 'setState()'
 		state = CONTROL_STATE.FIXING; //variable 'StAtE' to 'state'		
 	}
 
 
-	public void FIX_Book(boolean isMustFix) { //variable 'MUST_fix' to 'isMustFix'
+	public void fixBook(boolean isMustFix) { //variable 'MUST_fix' to 'isMustFix', method 'FIX_Book()' to 'fixBook()'
 		if (!state.equals(CONTROL_STATE.FIXING)) { //variable 'StAtE' to 'state'
 			throw new RuntimeException("FixBookControl: cannot call fixBook except in FIXING state");
 		}	
 		if (isMustFix) { //variable 'MUST_fix' to 'isMustFix'
-			library.Repair_BOOK(curBook); //variable 'Cur_Book' to 'curBook', variable 'LIB' to 'library'
+			library.repairBook(curBook); //variable 'Cur_Book' to 'curBook', variable 'LIB' to 'library', method 'Repair_BOOK()' to 'repairBook()'
 		}
 		curBook = null; //variable 'Cur_Book' to 'curBook'
-		fixBookUi.Set_State(FixBookUI.UI_STATE.READY); //variable 'UI' to 'fixBookUi'
+		fixBookUi.setState(FixBookUI.UI_STATE.READY); //variable 'UI' to 'fixBookUi', method 'Set_State()' to 'setState()'
 		state = CONTROL_STATE.READY; //variable 'StAtE' to 'state'	 	
 	}
 
 	
-	public void SCannING_COMplete() {
+	public void scanningComplete() { //method 'SCannING_COMplete()' to 'scanningComplete()'
 		if (!state.equals(CONTROL_STATE.READY)) { //variable 'StAtE' to 'state'
 			throw new RuntimeException("FixBookControl: cannot call scanningComplete except in READY state");
 		}	
-		fixBookUi.Set_State(FixBookUI.UI_STATE.COMPLETED); //variable 'UI' to 'fixBookUi'		
+		fixBookUi.setState(FixBookUI.UI_STATE.COMPLETED); //variable 'UI' to 'fixBookUi', method 'Set_State()' to 'setState()'	
 	}
 
 
