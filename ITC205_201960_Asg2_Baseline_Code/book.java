05ef104da396d53a6636bde8da8ab909e5383332
codewarriors03 <52955769+codewarriors03@users.noreply.github.com>
2019-08-18 16:22:21 +1000

    Update book.java
    Implemented all methods changes and give proper naming of it.

--- ITC205_201960_Asg2_Baseline_Code/book.java
+++ ITC205_201960_Asg2_Baseline_Code/book.java
@@ -5,59 +5,59 @@ import java.io.Serializable;
 public class book implements Serializable {
 	
 	private String title;	// 'TITLE' changed to 'title' -> Author Ankit
-	private String author;	// 'AUTHOR' changed to 'author' -> Author Ankit
-	private String callNo;	// 'CALLNO' changed to 'callNo' -> Author Ankit
-	private int id;	// 'ID' changed to 'id' -> Author Ankit
+	private String author;	// 'AUTHOR' changed to 'author'
+	private String callNo;	// 'CALLNO' changed to 'callNo'
+	private int id;	// 'ID' changed to 'id'
 	
 	private enum STATE { AVAILABLE, ON_LOAN, DAMAGED, RESERVED };
 	private STATE state;	// 'State' changed to 'state'
 	
 	
 	public book(String author, String title, String callNo, int id) {
-		this.author = author;	// 'AUTHOR' changed to 'author' -> Author Ankit
-		this.title = title;	// 'TITLE' changed to 'title' -> Author Ankit
-		this.callNo = callNo;	// 'CALLNO' changed to 'callNo' -> Author Ankit
-		this.id = id;	// 'ID' changed to 'id' -> Author Ankit
+		this.author = author;	// 'AUTHOR' changed to 'author'
+		this.title = title;	// 'TITLE' changed to 'title'
+		this.callNo = callNo;	// 'CALLNO' changed to 'callNo'
+		this.id = id;	// 'ID' changed to 'id'
 		this.state = STATE.AVAILABLE;	// 'State' changed to 'state'
 	}
 	
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("Book: ").append(id).append("\n")	// 'ID' changed to 'id' -> Author Ankit
-		  .append("  Title:  ").append(title).append("\n")	// 'TITLE' changed to 'title' -> Author Ankit
-		  .append("  Author: ").append(author).append("\n")	// 'AUTHOR' changed to 'author' -> Author Ankit
-		  .append("  CallNo: ").append(callNo).append("\n")	// 'CALLNO' changed to 'callNo' -> Author Ankit
+		sb.append("Book: ").append(id).append("\n")	// 'ID' changed to 'id'
+		  .append("  Title:  ").append(title).append("\n")	// 'TITLE' changed to 'title'
+		  .append("  Author: ").append(author).append("\n")	// 'AUTHOR' changed to 'author'
+		  .append("  CallNo: ").append(callNo).append("\n")	// 'CALLNO' changed to 'callNo'
 		  .append("  State:  ").append(state);	// 'State' changed to 'state'
 		
 		return sb.toString();
 	}
 
-	public Integer ID() {
-		return id;	// 'ID' changed to 'id' -> Author Ankit
+	public Integer getId() {	// 'ID' changed to 'getId'
+		return id;	// 'ID' changed to 'id'
 	}
 
-	public String TITLE() {
-		return title;	// 'TITLE' changed to 'title' -> Author Ankit
+	public String getTitle() {	// 'TITLE' changed to 'getTitle'
+		return title;	// 'TITLE' changed to 'title'
 	}
 
 
 	
-	public boolean AVAILABLE() {
+	public boolean isAvailable() {	// 'AVAILABLE' change to 'isAvailable'
 		return state == STATE.AVAILABLE;	// 'State' changed to 'state'
 	}
 
 	
-	public boolean On_loan() {
+	public boolean isOnLoan() {	// 'On_Loan' changed to 'isOnLoan'
 		return State == STATE.ON_LOAN;	// 'State' changed to 'state'
 	}
 
 	
-	public boolean IS_Damaged() {
+	public boolean isDamaged() {	// 'IS_Damaged' changed to 'isDamaged'
 		return State == STATE.DAMAGED;	// 'State' changed to 'state'
 	}
 
 	
-	public void Borrow() {
+	public void isBorrow() {	// 'Borrow' changed to 'isBorrow'
 		if (state.equals(STATE.AVAILABLE)) {	// 'State' changed to 'state'
 			state = STATE.ON_LOAN;	// 'State' changed to 'state'
 		}
@@ -68,7 +68,7 @@ public class book implements Serializable {
 	}
 
 
-	public void Return(boolean DAMAGED) {
+	public void isReturn(boolean DAMAGED) {		// 'Return' changed to 'Return'
 		if (state.equals(STATE.ON_LOAN)) {	// 'State' changed to 'state'
 			if (DAMAGED) {
 				state = STATE.DAMAGED;	// 'State' changed to 'state'
@@ -83,7 +83,7 @@ public class book implements Serializable {
 	}
 
 	
-	public void Repair() {
+	public void isRepair() {	// 'Repair' changed to 'isRepair'
 		if (state.equals(STATE.DAMAGED)) {	// 'State' changed to 'state'
 			state = STATE.AVAILABLE;	// 'State' changed to 'state'
 		}
